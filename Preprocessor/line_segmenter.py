4f452def7028d9f545e90925ba8e465dcc21ea47
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-06-03 16:11:47 +0200

    Linestarts found through computing minima between peaks and taking the middle minimal value as a starting point.

--- Preprocessor/line_segmenter.py
+++ Preprocessor/line_segmenter.py
@@ -34,13 +34,17 @@ def count_transistions(row):
 
 def find_line_starts(projection):
     """
-    Finds the start of line segmentation by taking the middle value between peaks.
+    Finds the start of line segmentation by taking the center minimal value between peaks.
     :param projection: list of projection values. Each element corresponds to a row in the original data.
     :return: list of row indices where segments start in the data.
     """
-
     peaks, properties = find_peaks(projection, prominence=1, distance=100)
-    minima = [int((peak + peaks[index+1])/2) for index, peak in enumerate(peaks[:-1])]
+
+    minima = []
+    for index, peak in enumerate(peaks[:-1]):
+        subset = projection[peaks[index]:peaks[index+1]]
+        local_minima = np.where(subset == np.amin(subset))[0]
+        minima.append(peak + local_minima[int(len(local_minima)/2)])
 
     return minima
 
@@ -49,25 +53,27 @@ if __name__ == '__main__':
 
     print("Number of processors: ", mp.cpu_count())
 
-    # Load and binarize image
-    img = cv2.imread('../data/test.jpg', 0)
-    img_binarized = binarize_image(img)
+    for image in range(0, 3):
 
-    # Compute projection & find line starts.
-    data = np.array(img_binarized)
-    projection = np.apply_along_axis(count_transistions, COLUMNS, data)
-    line_starts = find_line_starts(projection)
+        # Load and binarize image
+        img = cv2.imread(f"../data/test{image}.jpg", 0)
+        img_binarized = binarize_image(img)
 
-    # Show projection
-    plt.plot(projection)
-    plt.plot(line_starts, projection[line_starts], "x")
-    plt.title("Ink-paper transition projection with line starts")
-    plt.ylabel("Number of transitions")
-    plt.xlabel("Pixel row number")
-    plt.savefig("./results/ink-paper-transition-projection-minima.jpg")
-    plt.show()
+        # Compute projection & find line starts.
+        data = np.array(img_binarized)
+        projection = np.apply_along_axis(count_transistions, COLUMNS, data)
+        line_starts = find_line_starts(projection)
 
-    # Add detected lines to image
-    line_starts += [start+1 for start in line_starts] + [start-1 for start in line_starts]
-    data[line_starts] = np.zeros(data.shape[COLUMNS])
-    out_segment_image = Image.fromarray(data).save('./results/test_image_with_segmentlines.jpg')
+        # Show projection
+        plt.plot(projection)
+        plt.plot(line_starts, projection[line_starts], "x")
+        plt.title("Ink-paper transition projection with line starts")
+        plt.ylabel("Number of transitions")
+        plt.xlabel("Pixel row number")
+        plt.savefig(f"./results/test{image}-projection-minima.jpg")
+        plt.show()
+
+        # Add detected lines to image
+        line_starts += [start+1 for start in line_starts] + [start-1 for start in line_starts]
+        data[line_starts] = np.zeros(data.shape[COLUMNS])
+        out_segment_image = Image.fromarray(data).save(f"./results/test{image}_segmentlines.jpg")
