8ca60cc3b32a9204c9d963743b9376ffb21b3ed6
ubervelocity <mihaitu.popescu@gmail.com>
2020-06-24 15:30:30 +0200

    Refactors cfg file

--- character_recognizer/cfg.py
+++ character_recognizer/cfg.py
@@ -5,11 +5,14 @@ optimizer = "rmsprop"  # {rmsprop, nadam, adam, adagrad}
 
 # Regularization
 batch_norm = True
+
 dropout = True
-early_stopping = True
-monitor = "loss"  # {loss, val_loss}
 drop_hidden = 0.25  # {0.2, 0.25, 0.5}
 drop_output = 0.5  # {0.2, 0.25, 0.5}
+
+early_stopping = True
+monitor = "loss"  # {loss, val_loss}
+patience = 3  # {1, 2, 3, 4}
 
 # Hyperparameters
 epochs = 30
