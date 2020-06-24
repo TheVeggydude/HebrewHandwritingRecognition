6f86f88256546161c4ffd8d7091e0e1595bb2b03
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-06-24 23:26:38 +0200

    Early version of sentence segment cropping.

--- Preprocessor/preprocessor.py
+++ Preprocessor/preprocessor.py
@@ -5,24 +5,12 @@ import time
 
 from scipy.signal import find_peaks
 from Preprocessor.utils import count_transitions
-from Preprocessor.path_finding import find_path
+from Preprocessor.path_finding import find_path, MOVES
 
 
 # Constants
 ROWS = 0
 COLUMNS = 1
-
-
-# def extract_path(x1, x2, y1, y2):
-#     upper_bound = min(x1)
-#     lower_bound = max(x2)
-#     extracted_image = np.ones((lower_bound - upper_bound, line_image.shape[1]), dtype=int) * 255
-#     extracted_image = line_image[upper_bound:lower_bound, :]
-#     for x, y in zip(x1, y1):
-#         extracted_image[0: x - upper_bound, y] = 255
-#     for x, y in zip(x2, y2):
-#         extracted_image[x - upper_bound:, y] = 255
-#     return extracted_image
 
 
 def binarize_image(image):
@@ -54,32 +42,77 @@ def find_line_starts(projection):
     return start_data
 
 
+def crop_segment(upper, lower, img_data, index=0):
+    """
+
+    :param upper:
+    :param lower:
+    :param img_data:
+    :return:
+    """
+
+    # Find boundary values
+    upper_bound = min(upper[:, 0]) if upper is not None else 0  # min because the highest row has the lowest value
+    lower_bound = max(lower[:, 0]) if lower is not None else img_data.shape[ROWS]-1
+
+    # Crop horizontally
+    cropped = img_data[upper_bound: lower_bound, :]
+
+    if upper is not None:
+        for bound in upper:
+            cropped[:bound[ROWS]-upper_bound, bound[COLUMNS]] = 255  # set everything above boundary to white
+
+    if lower is not None:
+        for bound in lower:
+            cropped[bound[ROWS]-upper_bound:, bound[COLUMNS]] = 255  # set everything below boundary to white
+
+    plt.imshow(cropped, 'gray')
+    plt.title(f"Segment {index}")
+    plt.show()
+
+    return cropped
+
+
+def segment(img):
+    # Load and binarize image
+    img_binarized = binarize_image(img)
+
+    # Compute projection & find line starts.
+    img_data = np.array(img_binarized)
+    print(f"Image shape: {img_data.shape}")
+    projection = np.apply_along_axis(count_transitions, COLUMNS, img_data)
+    line_start_data = find_line_starts(projection)
+
+    # Find all the segments
+    segmentation_lines = [None for elem in line_start_data]  # to store the segmentation lines
+    for index, start in enumerate(line_start_data):
+        t = time.time()
+        print(f"Finding path for line at row {start} ({index + 1}/{len(line_start_data)}).")
+
+        segmentation_lines[index] = find_path(start[0], start[1], img_data)
+        print(f"Path found in {time.time() - t} seconds!")
+
+        plt.plot(segmentation_lines[index][:, COLUMNS], segmentation_lines[index][:, ROWS])
+
+    # Plot the segmentation
+    plt.imshow(img_data, 'gray')
+    plt.title(f"Image {i} segmentation")
+    plt.show()
+
+    # Actually split the image into segments
+    upper = None
+    for index, lower in enumerate(segmentation_lines):
+        crop_segment(upper, lower, img_data, index)
+        upper = lower
+
+        if index == len(segmentation_lines) - 1:
+            print("final one")
+            crop_segment(upper, None, img_data, index+1)
+
+
 if __name__ == '__main__':
 
-    for i in range(0, 3):
-        # Load and binarize image
+    for i in range(2, 3):
         print(f"Working on test image {i}")
         img = cv2.imread(f"../data/test{i}.jpg", 0)
-        img_binarized = binarize_image(img)
-
-        # Compute projection & find line starts.
-        img_arr = np.array(img_binarized)
-        print(f"Image shape: {img_arr.shape}")
-        projection = np.apply_along_axis(count_transitions, COLUMNS, img_arr)
-        line_start_data = find_line_starts(projection)
-
-        for index, start in enumerate(line_start_data):
-            t = time.time()
-            # do stuff
-
-            print(f"Finding path for line at row {start} ({index+1}/{len(line_start_data)}).")
-            path = find_path(start[0], start[1], img_arr)
-            print(f"Path found in {time.time() - t} seconds!")
-
-            # TODO verify path integrity
-            plt.plot(path[:, COLUMNS], path[:, ROWS])
-        plt.imshow(img_arr, 'gray')
-        plt.show()
-
-        # plt.imshow(extracted_image, 'gray', vmin=0)
-        # plt.show()
+        segment(img)
