7cee4e13901c65c4d0405bfe0b22ea2dd6b861e1
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-06-28 14:05:10 +0200

    PEP8-i-fied the project

--- pipeline/character_recognizer/train_vgg.py
+++ pipeline/character_recognizer/train_vgg.py
@@ -1,6 +1,7 @@
 # set the matplotlib backend so figures can be saved in the background
 import matplotlib
 import cfg
+
 matplotlib.use("Agg")
 
 from sklearn.preprocessing import LabelBinarizer
@@ -28,83 +29,83 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras import backend as K
 
+
 class SmallVGGNet:
-	@staticmethod
-	def build(width, height, depth, classes):
+    @staticmethod
+    def build(width, height, depth, classes):
 
-		# initialize the model along with the input shape to be
-		# "channels last" and the channels dimension itself
-		model = Sequential()
-		inputShape = (height, width, depth)
-		activation = cfg.activation
-		chanDim = -1
+        # initialize the model along with the input shape to be
+        # "channels last" and the channels dimension itself
+        model = Sequential()
+        inputShape = (height, width, depth)
+        activation = cfg.activation
+        chanDim = -1
 
-		# if we are using "channels first", update the input shape
-		# and channels dimension
-		if K.image_data_format() == "channels_first":
-			inputShape = (depth, height, width)
-			chanDim = 1
+        # if we are using "channels first", update the input shape
+        # and channels dimension
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
 
-# CONV => RELU => POOL layer set
-		model.add(Conv2D(32, (3, 3), padding="same",
-			input_shape=inputShape))
-		model.add(Activation(activation))
-		if cfg.batch_norm:
-			model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		if cfg.dropout:
-			model.add(Dropout(cfg.drop_hidden))
-        
-# (CONV => RELU) * 2 => POOL layer set
-		model.add(Conv2D(64, (3, 3), padding="same"))
-		model.add(Activation(activation))
-		if cfg.batch_norm:
-			model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(64, (3, 3), padding="same"))
-		model.add(Activation(activation))
-		if cfg.batch_norm:
-			model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		if cfg.dropout:
-			model.add(Dropout(cfg.drop_hidden))
+        # CONV => RELU => POOL layer set
+        model.add(Conv2D(32, (3, 3), padding="same",
+                         input_shape=inputShape))
+        model.add(Activation(activation))
+        if cfg.batch_norm:
+            model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        if cfg.dropout:
+            model.add(Dropout(cfg.drop_hidden))
 
+        # (CONV => RELU) * 2 => POOL layer set
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation(activation))
+        if cfg.batch_norm:
+            model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation(activation))
+        if cfg.batch_norm:
+            model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        if cfg.dropout:
+            model.add(Dropout(cfg.drop_hidden))
 
-# (CONV => RELU) * 3 => POOL layer set
-		model.add(Conv2D(128, (3, 3), padding="same"))
-		model.add(Activation(activation))
-		if cfg.batch_norm:
-			model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(128, (3, 3), padding="same"))
-		model.add(Activation(activation))
-		if cfg.batch_norm:
-			model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(128, (3, 3), padding="same"))
-		model.add(Activation(activation))
-		if cfg.batch_norm:
-			model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		if cfg.dropout:
-			model.add(Dropout(cfg.drop_hidden))
+        # (CONV => RELU) * 3 => POOL layer set
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation(activation))
+        if cfg.batch_norm:
+            model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation(activation))
+        if cfg.batch_norm:
+            model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation(activation))
+        if cfg.batch_norm:
+            model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        if cfg.dropout:
+            model.add(Dropout(cfg.drop_hidden))
 
-# first (and only) set of FC => RELU layers
-		model.add(Flatten())
-		model.add(Dense(512))
-		model.add(Activation(activation))
-		if cfg.batch_norm:
-			model.add(BatchNormalization())
-		if cfg.dropout:
-			model.add(Dropout(cfg.drop_output))
-		
-		# softmax classifier
-		model.add(Dense(classes))
-		model.add(Activation("softmax"))
+        # first (and only) set of FC => RELU layers
+        model.add(Flatten())
+        model.add(Dense(512))
+        model.add(Activation(activation))
+        if cfg.batch_norm:
+            model.add(BatchNormalization())
+        if cfg.dropout:
+            model.add(Dropout(cfg.drop_output))
 
-		# return the constructed network architecture
-		return model
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        # return the constructed network architecture
+        return model
+
 
 # Resizes images to 64x64 while keeping aspect ratio by adding white pixels
 def Reformat_Image(ImageFilePath):
-
     from PIL import Image
     image = Image.open(ImageFilePath, 'r')
     image_size = image.size
@@ -113,22 +114,23 @@ def Reformat_Image(ImageFilePath):
 
     bigside = 64
 
-    background = Image.new('RGB', (bigside, bigside), (255,255,255))
-    offset = (int(round(((bigside - width) / 2), 0)), int(round(((bigside - height) / 2),0)))
+    background = Image.new('RGB', (bigside, bigside), (255, 255, 255))
+    offset = (int(round(((bigside - width) / 2), 0)), int(round(((bigside - height) / 2), 0)))
 
     background.paste(image, offset)
     return background
 
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
-	help="path to input dataset of images")
+                help="path to input dataset of images")
 ap.add_argument("-m", "--model", required=True,
-	help="path to output trained model")
+                help="path to output trained model")
 ap.add_argument("-l", "--label-bin", required=True,
-	help="path to output label binarizer")
+                help="path to output label binarizer")
 ap.add_argument("-p", "--plot", required=True,
-	help="path to output accuracy/loss plot")
+                help="path to output accuracy/loss plot")
 args = vars(ap.parse_args())
 
 # initialize the data and labels
@@ -143,25 +145,25 @@ random.shuffle(imagePaths)
 
 # loop over the input images
 for imagePath in imagePaths:
-	# load the image, resize it to 64x64 pixels (the required input
-	# spatial dimensions of SmallVGGNet), and store the image in the
-	# data list
+    # load the image, resize it to 64x64 pixels (the required input
+    # spatial dimensions of SmallVGGNet), and store the image in the
+    # data list
 
-	# Resizes images to 64x64 while keeping aspect ratio
-	if cfg.maintain_aspect_ratio:		
-		image = np.array(Reformat_Image(imagePath))
-		image = image[:, :, ::-1].copy()
-	else:
-		image = cv2.imread(imagePath)
-		image = cv2.resize(image, (64, 64))
+    # Resizes images to 64x64 while keeping aspect ratio
+    if cfg.maintain_aspect_ratio:
+        image = np.array(Reformat_Image(imagePath))
+        image = image[:, :, ::-1].copy()
+    else:
+        image = cv2.imread(imagePath)
+        image = cv2.resize(image, (64, 64))
 
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	data.append(image)
-	
-	# extract the class label from the image path and update the
-	# labels list
-	label = imagePath.split(os.path.sep)[-2]
-	labels.append(label)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    data.append(image)
+
+    # extract the class label from the image path and update the
+    # labels list
+    label = imagePath.split(os.path.sep)[-2]
+    labels.append(label)
 
 # scale the raw pixel intensities to the range [0, 1]
 data = np.array(data, dtype="float") / 255.0
@@ -172,7 +174,7 @@ labels = np.array(labels)
 # partition the data into training and testing splits using 75% of
 # the data for training and the remaining 25% for testing
 (trainX, testX, trainY, testY) = train_test_split(data,
-	labels, test_size=0.25, random_state=42)
+                                                  labels, test_size=0.25, random_state=42)
 
 # convert the labels from integers to vectors (for 2-class, binary
 # classification you should use Keras' to_categorical function
@@ -184,11 +186,11 @@ testY = lb.transform(testY)
 
 # construct the image generator for data augmentation
 aug = ImageDataGenerator(rotation_range=15, width_shift_range=0.1,
-	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2, fill_mode="nearest")
+                         height_shift_range=0.1, shear_range=0.2, zoom_range=0.2, fill_mode="nearest")
 
 # initialize our VGG-like Convolutional Neural Network
 model = SmallVGGNet.build(width=64, height=64, depth=1,
-	classes=len(lb.classes_))
+                          classes=len(lb.classes_))
 
 # initialize our initial learning rate, # of epochs to train for,
 # and batch size
@@ -204,23 +206,23 @@ optimizer = cfg.optimizer
 early_stopping = EarlyStopping(monitor=cfg.monitor, patience=cfg.patience)
 callbacks = None
 if cfg.early_stopping:
-	callbacks = early_stopping
+    callbacks = early_stopping
 model.compile(loss="categorical_crossentropy", optimizer=optimizer,
-	metrics=["accuracy"])
+              metrics=["accuracy"])
 
 # train the network
 H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
-	validation_data=(testX, testY), steps_per_epoch=len(trainX) // BS,
-	epochs=EPOCHS, callbacks=early_stopping)
-	
-#H = model.fit(trainX, trainY, validation_data=(testX, testY),
+                        validation_data=(testX, testY), steps_per_epoch=len(trainX) // BS,
+                        epochs=EPOCHS, callbacks=early_stopping)
+
+# H = model.fit(trainX, trainY, validation_data=(testX, testY),
 #	epochs=EPOCHS, batch_size=BS)
 
 # evaluate the network
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=32)
 print(classification_report(testY.argmax(axis=1),
-	predictions.argmax(axis=1), target_names=lb.classes_))
+                            predictions.argmax(axis=1), target_names=lb.classes_))
 
 # Save the model
 print("[INFO] serializing network and label binarizer...")
@@ -243,7 +245,7 @@ plt.savefig(args["plot"])
 # save the model and label binarizer to disk
 print("[INFO] serializing network and label binarizer...")
 
-#model.save(args["model"])
+# model.save(args["model"])
 f = open(args["label_bin"], "wb")
 f.write(pickle.dumps(lb))
 f.close()
